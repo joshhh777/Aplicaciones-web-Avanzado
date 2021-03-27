97ffa7b17c2b29142b0a51676c3a01107e528fd6
unknown <jose.cancino@tecsup.edu.pe>
2021-03-26 22:36:52 -0500

    Creando componentes Header, content y Total

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './componentes/header.js'
+import Content from './componentes/content.js'
+import Total from './componentes/total.js'
+
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -12,17 +16,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
